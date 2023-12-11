d1eeab046ce34d46e7bdff13ea1542cb04464ed6
javi <fordrsmax@gmail.com>
2023-12-11 17:27:11 +0100

    add basic server

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors')
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+const io = new Server(server,{
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  });
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+io.on('connection', (socket) => {
+
+    console.log('connected from client',socket.id);
+    socket.emit('foo',"hello client ")
+
+    socket.on('create-something', (value) => {
+      console.log('echo from server ');
+      console.log('client send data: ' ,value)
+      let msg = "server catched data " + value +""
+      socket.emit('foo',msg)
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnect from client');
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`listening on port:${port}`);
+});
