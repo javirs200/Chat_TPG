cfb208830fea37cec2a486df94b01a45820b7e6f
javi <fordrsmax@gmail.com>
2023-12-13 10:48:08 +0100

    structure components and chage prop drilling to context

---
+++ client/src/components/Main/MyForm/MyForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { socket } from '../../../config/socket';
+
+export function MyForm() {
+  const [value, setValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    socket.timeout(2000).emit('clientMessage', value, () => {
+      setIsLoading(false);
+    });
+  }
+
+  return (
+    <form onSubmit={ onSubmit }>
+      <input onChange={ e => setValue(e.target.value) } />
+
+      <button type="submit" disabled={ isLoading }>Submit</button>
+    </form>
+  );
+}
