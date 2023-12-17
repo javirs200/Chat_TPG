865bfe0ffe6e9eb26b3c3150460459f67867b9d4
rsmax <fordrsmax@gmail.com>
2023-12-17 12:55:44 +0100

    remove api messages (it travels on sockets)

--- routes/messages.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const messagesRouter = express.Router();
-const messages = require('../controllers/messagesController');
-
-messagesRouter.post('/send', messages.send);
-
-
-module.exports = messagesRouter;
