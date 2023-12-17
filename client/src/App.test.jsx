bc186b85d5c58ac5cc39d7262649faaa519dcf8e
rsmax <fordrsmax@gmail.com>
2023-12-17 19:07:04 +0100

    edit app.jsx test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App.js'
+import App from './App.jsx'
 
 test('Learn React link is visible', () => {
     render(<App />);
