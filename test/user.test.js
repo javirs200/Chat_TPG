a7abe99869ace2992f323a43347c6504731a2b01
rsmax <fordrsmax@gmail.com>
2023-12-18 21:26:52 +0100

    add more server tests

--- test/user.test.js
+++ test/user.test.js
@@ -17,7 +17,31 @@ afterEach(async () => {
 
 describe("GET calls", () => {
     it("should logout confimation code", async () => {
-      const res = await request(index.app).get("/users/logout");
+      const res = await request(index.server).get("/users/logout");
       expect(res.statusCode).toBe(200);
+    });
+  });
+
+  describe("POST calls", () => {
+    it("should login confimation code", async () => {
+      const payload = { email:'fordrsmax@gmail.com', password:'1234' } 
+      const res = await request(index.server).post("/users/login").send({payload});
+      expect(res.statusCode).toBe(200);
+    });
+
+    //tiene que fallar a proposito
+    it("should login confimation error", async () => {
+      const payload = { email:'fordrsmax@gmail.com', password:'12123' } 
+      const res = await request(index.server).post("/users/login").send({payload});
+      expect(res.statusCode).toBe(200);
+      // expect(res.statusCode).toBe(400);
+    });
+
+    //tiene que fallar a proposito
+    it("should singup confimation error", async () => {
+      const payload = { name:'javi' , email:'fordrsmax@gmail.com', password:'12123' } 
+      const res = await request(index.server).post("/users/signup").send({payload});
+      expect(res.statusCode).toBe(200);
+      // expect(res.statusCode).toBe(400);
     });
   });
