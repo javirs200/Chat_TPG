4af0ed0d3198883b501429ac8f485e0bb7f5f1b3
rsmax <fordrsmax@gmail.com>
2023-12-18 22:28:55 +0100

    fix server users endpoint test

--- test/user.test.js
+++ test/user.test.js
@@ -24,22 +24,25 @@ describe("GET calls", () => {
 
   describe("POST calls", () => {
     it("should login confimation code", async () => {
-      const payload = { email:'fordrsmax@gmail.com', password:'1234' } 
-      const res = await request(index.server).post("/users/login").set('Content-type', 'application/json').send({payload});
+      const res = await request(index.server).post("/users/login").set('Content-type', 'application/json').send({ email:'fordrsmax@gmail.com', password:'1234' });
       expect(res.statusCode).toBe(200);
     });
 
     //tiene que fallar a proposito
-    it("should login confimation error", async () => {
-      const payload = { email:'fordrsmax@gmail.com', password:'12123' } 
-      const res = await request(index.server).post("/users/login").set('Content-type', 'application/json').send({payload});
-      expect(res.statusCode).toBe(400);
+    it("intento login con mala contraseña , should be 404", async () => { 
+      const res = await request(index.server).post("/users/login").set('Content-type', 'application/json').send({ email:'fordrsmax@gmail.com', password:'adsd1234' });
+      expect(res.statusCode).toBe(404);//404 not found -> no hay usuario con esas credenciales
+    });
+
+    //tiene que fallar a proposito
+    it("intento login con usuario no registrado , should be 404", async () => { 
+      const res = await request(index.server).post("/users/login").set('Content-type', 'application/json').send({ email:'b@b.com', password:'1234' });
+      expect(res.statusCode).toBe(404);//404 not found -> no hay usuario con esas credenciales
     });
 
     //tiene que fallar a proposito
     it("should singup confimation error", async () => {
-      const payload = { name:'javi' , email:'fordrsmax@gmail.com', password:'12123' } 
-      const res = await request(index.server).post("/users/signup").set('Content-type', 'application/json').send({payload});
+      const res = await request(index.server).post("/users/signup").set('Content-type', 'application/json').send( { name:'javi' , email:'fordrsmax@gmail.com', password:'12123' } );
       expect(res.statusCode).toBe(400);
     });
   });
