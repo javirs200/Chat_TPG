53a8e8a83a21dc8073c9f9d8d00748f2fff9704b
rsmax <fordrsmax@gmail.com>
2023-12-18 20:54:10 +0100

    add client test cases

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen  } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import App from './App.jsx'
 
-test('render shomething', () => {
+
+test('render shomething on App component', () => {
     render(<App />);
     const HomeComponent = screen.queryByText('Chat TPG');
-    expect(HomeComponent)
+    expect(HomeComponent).toBeVisible();
   });
--- client/src/components/Main/Chat/MessageBox/Message/Message.test.jsx
+++ client/src/components/Main/Chat/MessageBox/Message/Message.test.jsx
@@ -1,19 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
-import Message from './Message';
+import Message from './Message.jsx';
 
 describe('test del componente mensaje',()=>{
 
-  test('render shomething', () => {
-    const user = 'usuario'
-    const message = 'este es mi mensaje'
-    const type = 0
-    const diference = 'Now'
-    render(<Message user={user} message={message} type={type} timestamp={diference} />);
-    const Message = screen.queryByText('este es mi mensaje');
-    console.log("elemento mensaje en los tes",Message);
-    expect(Message).toBeVisible();
+  const user = 'usuario'
+  const message = 'este es mi mensaje'
+  const type = 0
+  const diference = 'Now'
+  
+  test('render messaje text', () => {
+    const { container } = render(<Message user={user} message={message} type={type} timestamp={diference} />);
+    const texto = container.querySelector('.texto');
+    expect(texto).toBeVisible();
+    expect(texto.innerHTML).toBe('este es mi mensaje');
   });
 
+  test('render usuario Capitalized ', () => {
+    const { container } = render(<Message user={user} message={message} type={type} timestamp={diference} />);
+    const nick = container.querySelector('.nick');
+    expect(nick).toBeVisible();
+    expect(nick.innerHTML).toBe('Usuario');
+  });
+
+  test('render diference text', () => {
+    const { container } = render(<Message user={user} message={message} type={type} timestamp={diference} />);
+    const timestamp = container.querySelector('.timestamp');
+    expect(timestamp).toBeVisible();
+    expect(timestamp.innerHTML).toBe('Now');
+  });
+
+  test('have correct class', () => {
+    const { container } = render(<Message user={user} message={message} type={type} timestamp={diference} />);
+    expect(container).toBeVisible();
+    expect(container.firstChild.classList.contains('Messaje0')).toBe(true)
+  });
 
 });
