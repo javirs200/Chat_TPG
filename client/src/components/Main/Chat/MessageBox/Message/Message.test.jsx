05fbfc19a246e15522c6945ee7166cd5cff2d961
javi <fordrsmax@gmail.com>
2023-12-18 17:46:39 +0100

    add more tests

--- client/src/components/Main/Chat/MessageBox/Message/Message.test.jsx
+++ client/src/components/Main/Chat/MessageBox/Message/Message.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react';
-import Messaje from './Message.jsx'
+import "@testing-library/jest-dom/extend-expect";
+import Message from './Message';
 
-test('render shomething', () => {
-    const user='usuario'
-    const message='este es mi mensaje'
-    const type=0 
-    render(<Messaje user={user} message={message} type={type}/>);
-    // const HomeComponent = screen.queryByText('Chat TPG');
-    // expect(HomeComponent).toBeVisible();
+describe('test del componente mensaje',()=>{
+
+  test('render shomething', () => {
+    const user = 'usuario'
+    const message = 'este es mi mensaje'
+    const type = 0
+    const diference = 'Now'
+    render(<Message user={user} message={message} type={type} timestamp={diference} />);
+    const Message = screen.queryByText('este es mi mensaje');
+    console.log("elemento mensaje en los tes",Message);
+    expect(Message).toBeVisible();
   });
+
+
+});
